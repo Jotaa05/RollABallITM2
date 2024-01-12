93000cdf054c60f253857050c737650c41925f09
João Pedro <155730247+Jotaa05@users.noreply.github.com>
2024-01-12 12:20:51 +0000

    Pontuação, Decoração

--- Assets/Scripts/Jogador.cs
+++ Assets/Scripts/Jogador.cs
@@ -1,19 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Jogador : MonoBehaviour    
 {
     [SerializeField]
     private float velocidade;
 
+    private int contador;
+
     private Rigidbody rb;
+
+    public TMP_Text contar;
+
+    public TMP_Text vitoria;
+
 
     // Start is called before the first frame update
     void Start()
     {
+        contador = 0;
         rb = GetComponent<Rigidbody>();
+        Contador();
+        vitoria.text = "";
+    }
+
+    void Contador()
+    {
+        contar.text = "Contador: "  + contador.ToString();  
+        if (contador >= 6)
+        {
+            vitoria.text = "Ganhou! Parabens";
+        }
     }
 
     private void FixedUpdate()
@@ -32,6 +53,8 @@ public class Jogador : MonoBehaviour
         if(other.gameObject.CompareTag("Coletavel"))
         {
             other.gameObject.SetActive(false);
+            contador = contador + 1;
+            Contador();
         }
     }
 
@@ -40,4 +63,6 @@ public class Jogador : MonoBehaviour
     {
         
     }
+
+
 }
